1e7ea7a409cce5452dd6256d82812983f60180e9
Rizu0007 <87184120+Rizu0007@users.noreply.github.com>
2023-03-23 16:20:34 +0500

    Profile

--- src/page/Profile.jsx
+++ src/page/Profile.jsx
@@ -19,17 +19,26 @@ import { Link } from 'react-router-dom'
 import ListingItem from './../components/ListingItem';
 
 const Profile = () => {
-  const [listings , setListings]=useState(null)
-  const [ loading , setloading]=useState(true)
-const auth=getAuth()
-const navigate=useNavigate();
-const [ChangeDetail , setChangeDetail]=useState(false)
-
-  const [formData , setformData]=useState({
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false);
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
-    } 
-  );
+  });
+  const { name, email } = formData;
+  function onLogout() {
+    auth.signOut();
+    navigate("/");
+  }
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
   async function onSubmit() {
     try {
       if (auth.currentUser.displayName !== name) {
@@ -50,42 +59,27 @@ const [ChangeDetail , setChangeDetail]=useState(false)
       toast.error("Could not update the profile details");
     }
   }
- 
-  const {name , email}=formData;
-
-  function onLogout(){
-    auth.signOut()
-    navigate("/")
-  }
-  function onChange(e){
-  setformData((prevState)=>({
-    ...prevState,
-    [e.target.id]:e.target.value,
-  }))
-  }
-
   useEffect(() => {
     async function fetchUserListings() {
-   
       const listingRef = collection(db, "listings");
       const q = query(
         listingRef,
         where("userRef", "==", auth.currentUser.uid),
         orderBy("timestamp", "desc")
       );
-      const querySnap=await getDocs(q);
-      let listings=[];
-      querySnap.forEach((doc)=>{
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
         return listings.push({
-          id:doc.id,
-          data:doc.data(),
-
-        })
-      })
-      setListings()    }
-    fetchUserListings(listings)
-    setloading(false)
-  },[auth.currentUser.uid]);
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
     
   return (
     <>
@@ -95,10 +89,10 @@ const [ChangeDetail , setChangeDetail]=useState(false)
         <form>
 
 
-      <input type='text' id='name' value={name} disabled={!ChangeDetail} 
+      <input type='text' id='name' value={name} disabled={!changeDetail} 
       onChange={onChange}
        className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
-        ChangeDetail && "bg-red-200 focus:bg-red-200"
+        changeDetail && "bg-red-200 focus:bg-red-200"
       }`} />
 
       <input type='email' id='email' value={email} disabled className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out' />
@@ -106,12 +100,12 @@ const [ChangeDetail , setChangeDetail]=useState(false)
      <p className="flex items-center ">
      Do you want to change your name?
      <span   onClick={()=>{
-      ChangeDetail && onSubmit() ;
+      changeDetail && onSubmit() ;
      setChangeDetail((prevState) =>!prevState)
     }}
      className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
      >
-     {ChangeDetail ? "Apply change" : "Edit"}
+     {changeDetail ? "Apply change" : "Edit"}
      </span>
    </p>
    <p onClick={onLogout} className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer">
@@ -120,22 +114,36 @@ const [ChangeDetail , setChangeDetail]=useState(false)
      </div>
 
      </form>
-       <button type='submit' className='w-full bg-blue-400 text-white uppercase px-7 py-7 text-sm font-medium rounded shadow-md hover:bg-blue-700 translate duration-200 ease-out hover:shadow-lg active:bg-red-300'>
+       <button type='submit' className='w-full bg-stone-700 text-white uppercase px-7 py-7 text-sm font-medium rounded shadow-md hover:bg-orange-600 translate duration-200 ease-out hover:shadow-lg active:bg-red-300'>
        <Link to='/CreateListing' className='flex justify-center items-center'>
        <FcHome className='mr-2 text-3xl bg-red-600 rounded-full p-1 border-2'/>
-       </Link>
-       
        Sell or Rent your Home
-
+       </Link>
        </button>
-       <h1>gh</h1>
     </div>
     </section>
     <div className="max-w-6xl px-3 mt-6 mx-auto">
-    
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold mb-6">
+              My Listings
+            </h2>
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+          
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </div>
+    
     </>
-  )
+  );
 }
 
 export default Profile
